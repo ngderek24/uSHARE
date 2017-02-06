fb34e96f672b5a9e5962500014aeff71242a190e
guilam34 <guillaumelam34@yahoo.com>
2017-02-06 11:31:25 -0800

    homepage wireframe

--- uSHARE/routes/index.js
+++ uSHARE/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// TODO: fix spotify url endpoint
+// TODO: pass template the approriate links based on user login status
+var links = [
+  				{ name:"Login via Spotify",
+  				  endpoint: "/auth" }
+  			] 
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'u S H A R E' });
+  res.render('index', { title: 'uSHARE',
+  						links: links
+  					});
 });
 
 module.exports = router;
