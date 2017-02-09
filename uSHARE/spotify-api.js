8d91e2c200f3674abd80a83c25111ec2b19dfc1f
Jason Yoon <jasony0206@gmail.com>
2017-02-09 00:39:36 -0800

    Request access token and refresh token after user login

--- uSHARE/spotify-api.js
+++ uSHARE/spotify-api.js
@@ -1,12 +1,15 @@
 var querystring = require('querystring');
+var request = require('request');
 
 function SpotifyApi() {}
 
 var clientID = '211aae652e324de8b0237d55d0fa3030';
 var clientSecret = '691fdcd98e054278aac41672f119f9dd';
-var redirectURI = 'http://localhost:3000/';
+var redirectURI = 'http://localhost:3000/spotifyTest/callback';
 var scopes = 'playlist-modify-public';
 var stateKey = 'spotify_auth_state';
+var accessToken = '';
+var refreshToken = '';
 
 SpotifyApi.prototype = {
   
@@ -26,6 +29,66 @@ SpotifyApi.prototype = {
         redirect_uri: redirectURI,
         state: state
       }));
+  },
+
+  requestAccessToken: function(req, res) {
+    // your application requests refresh and access tokens
+    // after checking the state parameter
+    var code = req.query.code || null;
+    var state = req.query.state || null;
+    var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+    if (state === null || state !== storedState) {
+      res.redirect('/#' +
+        querystring.stringify({
+          error: 'state_mismatch'
+        }));
+    } else {
+      res.clearCookie(stateKey);
+      var authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        form: {
+          code: code,
+          redirect_uri: redirectURI,
+          grant_type: 'authorization_code'
+        },
+        headers: {
+          'Authorization': 'Basic ' + (new Buffer(clientID + ':' + clientSecret).toString('base64'))
+        },
+        json: true
+      };
+
+      request.post(authOptions, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+
+          accessToken = body.access_token;
+          refreshToken = body.refresh_token;
+
+          var options = {
+            url: 'https://api.spotify.com/v1/me',
+            headers: { 'Authorization': 'Bearer ' + accessToken },
+            json: true
+          };
+
+          // Use the access token to access the Spotify Web API
+          // request.get(options, function(error, response, body) {
+          //   console.log(body);
+          // });
+
+          // We can also pass the token to the browser to make requests from there
+          res.redirect('/#' +
+            querystring.stringify({
+              access_token: accessToken,
+              refresh_token: refreshToken
+            }));
+        } else {
+          res.redirect('/#' +
+            querystring.stringify({
+              error: 'invalid_token'
+            }));
+        }
+      });
+    }
   }
 };
 
