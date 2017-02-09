6eadd99ecc1a66e6421eee9ddbc054eff2171426
Jason Yoon <jasony0206@gmail.com>
2017-02-09 00:13:00 -0800

    Prompt user to login to spotify

--- uSHARE/spotify-api.js
+++ uSHARE/spotify-api.js
@@ -1,10 +1,43 @@
-function SpotifyApi(clientID, clientSecret, redirectURI) {
-  this._clientID = clientID;
-  this._clientSecret = clientSecret;
-  this._redirectURI = redirectURI;
-}
+var querystring = require('querystring');
+
+function SpotifyApi() {}
+
+var clientID = '211aae652e324de8b0237d55d0fa3030';
+var clientSecret = '691fdcd98e054278aac41672f119f9dd';
+var redirectURI = 'http://localhost:3000/';
+var scopes = 'playlist-modify-public';
+var stateKey = 'spotify_auth_state';
 
 SpotifyApi.prototype = {
+  
+  setup: function() {},
+
+  // Prompts user to login to spotify
+  promptLogin: function(req, res) {
+    var state = generateRandomString(16);
+    res.cookie(stateKey, state);
+
+    // Request authorization for app
+    res.redirect('https://accounts.spotify.com/authorize?' +
+      querystring.stringify({
+        response_type: 'code',
+        client_id: clientID,
+        scope: scopes,
+        redirect_uri: redirectURI,
+        state: state
+      }));
+  }
 };
+
+// Generates a random string containing numbers and letters
+function generateRandomString(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
 
 module.exports = SpotifyApi;
