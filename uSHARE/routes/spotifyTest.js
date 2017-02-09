3fb2cad1fc5d9247bd72c8bab50dd843c5dbccdb
Jason Yoon <jasony0206@gmail.com>
2017-02-09 00:13:00 -0800

    Prompt user to login to spotify

--- uSHARE/routes/spotifyTest.js
+++ uSHARE/routes/spotifyTest.js
@@ -2,13 +2,14 @@ var SpotifyApi = require('../spotify-api');
 var express = require('express');
 var router = express.Router();
 
+var spotifyApi = new SpotifyApi();
+spotifyApi.setup();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('spotifyTest', { title: 'Spotify API Test' });
-
-  var spotifyApi = new SpotifyApi('211aae652e324de8b0237d55d0fa3030',
-                                  '691fdcd98e054278aac41672f119f9dd',
-                                  'http://localhost:3000/');
 });
+
+router.get('/login', spotifyApi.promptLogin);
 
 module.exports = router;
