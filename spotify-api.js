8d74a1ff1c03336052ab8e2427425613143b5dae
guilam34 <guillaumelam34@yahoo.com>
2017-03-01 13:07:37 -0800

    always prompt spotify authorization when a user has logged out previously

--- spotify-api.js
+++ spotify-api.js
@@ -3,9 +3,21 @@ var request = require('request');
 
 function SpotifyApi() {}
 
+// /*
+//   Dev credentials
+// */
 var clientID = '211aae652e324de8b0237d55d0fa3030';
 var clientSecret = '691fdcd98e054278aac41672f119f9dd';
 var redirectURI = 'http://localhost:3000/spotifyTest/callback';
+
+/*
+  Deploy credentials: remember to swap in 
+*/
+// var clientID = 'b31f86324a784e9db76255fc4467363d';
+// var clientSecret = 'aa7c45b74ed44e079e04abe681b48874';
+// var redirectURI = 'https://radiant-peak-71546.herokuapp.com/spotifyTest/callback';
+
+
 var scopes = 'playlist-modify-public playlist-modify-private playlist-read-private playlist-read-collaborative';
 var stateKey = 'spotify_auth_state';
 var accessToken = '';
@@ -28,7 +40,8 @@ SpotifyApi.prototype = {
         client_id: clientID,
         scope: scopes,
         redirect_uri: redirectURI,
-        state: state
+        state: state,
+        show_dialog: true,
       }));
   },
 
